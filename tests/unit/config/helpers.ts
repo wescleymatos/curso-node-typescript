6a55aaa9c756ca23f2df0b3ba6afdcfcce89a1fe
Wescley Matos <wescleymatos@gmail.com>
2018-06-17 10:50:00 -0300

    Configuração do ambiente de testes

---
+++ tests/unit/config/helpers.ts
@@ -0,0 +1,18 @@
+import * as mocha from 'mocha';
+import * as Chai from 'chai';
+import * as td from 'testdouble';
+const supertest = require('supertest');
+
+import App from '../../../server/api/App';
+
+const app = App;
+const request = supertest;
+const expect = Chai.expect;
+const testDouble = td;
+
+export {
+  app,
+  expect,
+  request,
+  testDouble
+}
